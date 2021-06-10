a21c33c3f0f6b73ddb67c7f2c03f572ff68e5813
Ines Bakhtaoui <inesbkht@gmail.com>
2021-06-10 14:50:16 +0200

    exercice 4.2 fait

---
+++ exercice4.2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 4.2</title>
+</head>
+<body>
+    <h1>Exercice 2 Partie 4 : Les fonctions PHP</h1>
+    <p>
+        <?php
+        $string = 'Voici une chaîne de caractères';
+        function stringFunction($string) : string 
+        {
+        return stringFunction($string);
+        }
+        echo $string;
+        ?>
+    </p>
+</body>
+</html>
